a5031cfeb21283de369dabb8d301765e9e1dd764
Stuart Fleisher <stufleisher@gmail.com>
2024-03-13 16:47:20 -0400

    added styling

--- src/pages/WelcomePage.tsx
+++ src/pages/WelcomePage.tsx
@@ -2,7 +2,8 @@ import { useContext } from "react";
 import { Link as RouterLink } from "react-router-dom";
 
 import Typography from "@mui/material/Typography";
-import IconButton from "@mui/material/IconButton";
+import Button from "@mui/material/Button";
+import Box from "@mui/material/Box";
 import Link from "@mui/material/Link";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -10,7 +11,7 @@ import { faPaste } from "@fortawesome/free-solid-svg-icons";
 
 import SimpleLayout from "../components/ui/SimpleLayout";
 import userContext from "../helpers/userContext";
-
+import "./WelcomePage.scss";
 
 function WelcomePage() {
 
@@ -19,17 +20,21 @@ function WelcomePage() {
     return (
 
         <SimpleLayout src="/images/banner01.jpg">
-            <Typography variant="h2">
-                {`Welcome to Parsley ${username}!`}
-            </Typography>
-            <Typography variant="body1">
-                Let's add your first recipe!
-            </Typography>
-            <Link component={RouterLink} to="/welcome">
-                <IconButton>
-                    <FontAwesomeIcon icon={faPaste} />
-                </IconButton>
-            </Link>
+            <Box className="WelcomePage">
+                <Typography component="h2" variant="h2">
+                    {`Welcome to Parsley ${username}!`}
+                </Typography>
+                <Link component={RouterLink} to="/recipes/create" underline="none">
+                    <Button
+                    variant="outlined"
+                        startIcon={<FontAwesomeIcon icon={faPaste} />}
+                    >
+                        <Typography variant="body1">
+                            Let's add your first recipe!
+                        </Typography>
+                    </Button>
+                </Link>
+            </Box>
 
         </SimpleLayout>
     );
