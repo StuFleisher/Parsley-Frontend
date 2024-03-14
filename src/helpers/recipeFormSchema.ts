9dab1a3673c0ee30b7dc17ea1f4c5bed0e0329c3
Stuart Fleisher <stufleisher@gmail.com>
2024-03-14 11:03:07 -0400

    Added user friendly validation messages

--- src/helpers/recipeFormSchema.ts
+++ src/helpers/recipeFormSchema.ts
@@ -2,22 +2,22 @@ import * as yup from 'yup';
 
 const recipeFormSchema = yup.object({
   recipeId: yup.number(),
-  name: yup.string().required(),
-  description: yup.string().required(),
+  name: yup.string().required("Name is required"),
+  description: yup.string().required("Description is required"),
   owner: yup.string(),
-  sourceName: yup.string().required(),
+  sourceName: yup.string().required("Recipe source is required"),
   sourceUrl: yup.string().url(),
   imageUrl: yup.string().url(),
   steps:yup.array().of(
     yup.object({
       stepNumber:yup.number(),
-      instructions: yup.string().required(),
+      instructions: yup.string().required("Instructions are required"),
       stepId:yup.number(),
       ingredients:yup.array().of(
         yup.object({
           ingredientId:yup.number(),
           amount: yup.string(),
-          description:yup.string().required(),
+          description:yup.string().required("Ingredient name is required"),
           instructionRef:yup.string(),
         })),
     })
