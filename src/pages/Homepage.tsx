89619003a23ba2ebf520539e3730606311d89a83
Stuart Fleisher <stufleisher@gmail.com>
2024-03-14 10:51:58 -0400

    fixed form width and conditional rendering bug

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,8 +1,10 @@
-
+import { useContext } from "react";
 import { Link as RouterLink } from "react-router-dom";
 
 import Lottie from "lottie-react";
 import explainerAnimation from "../animations/recipeTransform.json";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlusCircle } from "@fortawesome/free-solid-svg-icons";
 
 import IconButton from "@mui/material/IconButton";
 import Link from "@mui/material/Link";
@@ -12,14 +14,105 @@ import Stack from "@mui/material/Stack";
 
 import UserRegistrationForm from "../components/userAuth/UserRegistrationForm";
 import './Homepage.scss';
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPlusCircle } from "@fortawesome/free-solid-svg-icons";
+import userContext from "../helpers/userContext";
 
 type props = {
     register: (userInfo: IUser) => Promise<void>,
 };
 
 function Homepage({ register }: props) {
+
+    const {username} = useContext(userContext)
+
+    const anonCTA = (
+        <Box
+            className="CTA"
+        >
+            <Stack
+                component="section"
+                direction={{ xs: "column-reverse", sm: "row" }}
+                className="CTA-container"
+            >
+                <Stack
+                    className="CTA-imagePanel imagePanel"
+                    alignItems="center"
+                    justifyContent="center"
+                >
+                    <Box
+                        component="img"
+                        src="/images/banner01.jpg">
+                    </Box>
+                    <Box className="CTA-form">
+                        <UserRegistrationForm register={register} />
+                    </Box>
+
+                </Stack>
+                <Stack
+                    className="CTA-headlinePanel headlinePanel-right"
+                    direction="column"
+                    alignItems="center"
+                    justifyContent="center"
+                    spacing={2}
+                >
+                    <Typography component="h1" variant="h1">
+                        Let's get Cookin'.
+                    </Typography>
+                    <Typography variant="body2">
+                        Sign up for free and change the way you cook forever.
+                    </Typography>
+                </Stack>
+            </Stack>
+        </Box>
+    );
+
+    const userCTA = (
+        <Box
+                className="CTA"
+            >
+                <Stack
+                    component="section"
+                    direction={{ xs: "column-reverse", sm: "row" }}
+                    className="CTA-container"
+                >
+                    <Stack
+                        className="CTA-imagePanel imagePanel"
+                        alignItems="center"
+                        justifyContent="center"
+                    >
+                        <Box
+                            component="img"
+                            src="/images/banner01.jpg">
+                        </Box>
+                        <Stack className="CTA-addRecipeLink" alignItems={"center"} justifyContent={"center"}>
+                            <Link component={RouterLink} to="/recipes/create" underline="none">
+                                <IconButton                                >
+                                    <FontAwesomeIcon icon={faPlusCircle} />
+                                </IconButton>
+                            </Link>
+                            <Typography variant="body1">
+                                Create a new recipe
+                            </Typography>
+                        </Stack>
+
+                    </Stack>
+                    <Stack
+                        className="CTA-headlinePanel headlinePanel-right"
+                        direction="column"
+                        alignItems="center"
+                        justifyContent="center"
+                        spacing={2}
+                    >
+                        <Typography component="h1" variant="h1">
+                            Let's get Cookin'.
+                        </Typography>
+                        <Typography variant="body2">
+                            Copy/Paste recipe text from anywhere to build beautifully formatted recipes!
+                        </Typography>
+                    </Stack>
+                </Stack>
+            </Box>
+    )
+
 
     return (
         <div className="Homepage">
@@ -54,7 +147,7 @@ function Homepage({ register }: props) {
                         src="/images/hero-banner.jpg">
                     </Box>
                     {/* <Box className="Homepage-animation"> */}
-                        <Lottie loop animationData={explainerAnimation} className="Homepage-animation"/>
+                    <Lottie loop animationData={explainerAnimation} className="Homepage-animation" />
                     {/* </Box> */}
 
                 </Stack>
@@ -109,92 +202,10 @@ function Homepage({ register }: props) {
 
             </Stack>
 
-            {/* Register SECTION */}
-            <Box
-                className="CTA"
-            >
-                <Stack
-                    component="section"
-                    direction={{ xs: "column-reverse", sm: "row" }}
-                    className="CTA-container"
-                >
-                    <Stack
-                        className="CTA-imagePanel imagePanel"
-                        alignItems="center"
-                        justifyContent="center"
-                    >
-                        <Box
-                            component="img"
-                            src="/images/banner01.jpg">
-                        </Box>
-                        <Box className="CTA-form">
-                            <UserRegistrationForm register={register} />
-                        </Box>
+            {/* CTA SECTION */}
 
-                    </Stack>
-                    <Stack
-                        className="CTA-headlinePanel headlinePanel-right"
-                        direction="column"
-                        alignItems="center"
-                        justifyContent="center"
-                        spacing={2}
-                    >
-                        <Typography component="h1" variant="h1">
-                            Let's get Cookin'.
-                        </Typography>
-                        <Typography variant="body2">
-                            Sign up for free and change the way you cook forever.
-                        </Typography>
-                    </Stack>
-                </Stack>
-            </Box>
+        {username ? userCTA : anonCTA}
 
-            {/* addRecipe SECTION */}
-            <Box
-                className="CTA"
-            >
-                <Stack
-                    component="section"
-                    direction={{ xs: "column-reverse", sm: "row" }}
-                    className="CTA-container"
-                >
-                    <Stack
-                        className="CTA-imagePanel imagePanel"
-                        alignItems="center"
-                        justifyContent="center"
-                    >
-                        <Box
-                            component="img"
-                            src="/images/banner01.jpg">
-                        </Box>
-                        <Stack className="CTA-addRecipeLink" alignItems={"center"} justifyContent={"center"}>
-                            <Link component={RouterLink} to="/recipes/create" underline="none">
-                                <IconButton                                >
-                                    <FontAwesomeIcon icon={faPlusCircle} />
-                                </IconButton>
-                            </Link>
-                            <Typography variant="body1">
-                                Create a new recipe
-                            </Typography>
-                        </Stack>
-
-                    </Stack>
-                    <Stack
-                        className="CTA-headlinePanel headlinePanel-right"
-                        direction="column"
-                        alignItems="center"
-                        justifyContent="center"
-                        spacing={2}
-                    >
-                        <Typography component="h1" variant="h1">
-                            Let's get Cookin'.
-                        </Typography>
-                        <Typography variant="body2">
-                            Copy/Paste recipe text from anywhere to build beautifully formatted recipes!
-                        </Typography>
-                    </Stack>
-                </Stack>
-            </Box>
 
 
 
