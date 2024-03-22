0d09a38495baad03c7aa0efb7e379e495bad2ea5
Stuart Fleisher <stufleisher@gmail.com>
2024-03-22 17:20:14 -0400

    SimpleLayout no longer uses absolute positioning

--- src/components/ui/SimpleLayout.tsx
+++ src/components/ui/SimpleLayout.tsx
@@ -1,41 +1,36 @@
 import React from "react";
 
-import Box from "@mui/material/Box";
 import Stack from "@mui/material/Stack";
-import Container from "@mui/material/Container";
 
 import "./SimpleLayout.scss";
 
 type props = {
     children: React.ReactNode,
     src: string;
+    menuHeight?: string;
 };
 
-function SimpleLayout({ src, children }: props) {
+function SimpleLayout({ src, children, menuHeight = "3rem" }: props) {
     return (
-        <Container className=" Page-container" maxWidth="xl">
+        <Stack
+            className="SimpleLayout"
+            direction="column"
+            justifyContent="center"
+            alignItems="center"
+            sx={{
+                backgroundImage: `url('${src}')`,
+                height: `calc(100vh - ${menuHeight})`
+            }}
+        >
             <Stack
-                className="SimpleLayout"
-                direction="column"
+                className="SimpleLayout-contents"
                 justifyContent="center"
                 alignItems="center"
             >
-                <Stack
-                    className="SimpleLayout-contents"
-                    justifyContent="center"
-                    alignItems="center"
-                >
-                    {children}
-                </Stack>
-                <Box
-                    className="SimpleLayout-background"
-                    component="img"
-                    src={src}>
-                </Box>
-
+                {children}
             </Stack>
+        </Stack>
 
-        </Container >
     );
 }
 
