f3fef6711cc48aaae22f66fb562569e2fb18e2e8
Stuart Fleisher <stufleisher@gmail.com>
2025-02-12 14:45:30 -0500

    Overhauled homepage design

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,10 +1,10 @@
-import { useContext } from "react";
+import { useContext, useRef } from "react";
 import { Link as RouterLink } from "react-router-dom";
 
 import Lottie from "lottie-react";
 import explainerAnimation from "../animations/recipeTransform.json";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPlusCircle } from "@fortawesome/free-solid-svg-icons";
+import { faCircleArrowDown, faPlusCircle } from "@fortawesome/free-solid-svg-icons";
 
 import IconButton from "@mui/material/IconButton";
 import Link from "@mui/material/Link";
@@ -22,7 +22,9 @@ type props = {
 
 function Homepage({ register }: props) {
 
-    const {username} = useContext(userContext)
+    const formatSection = useRef<HTMLDivElement | null>(null);
+
+    const { username } = useContext(userContext);
 
     const anonCTA = (
         <Box
@@ -67,52 +69,51 @@ function Homepage({ register }: props) {
 
     const userCTA = (
         <Box
-                className="CTA"
+            className="CTA"
+        >
+            <Stack
+                component="section"
+                direction={{ xs: "column-reverse", sm: "row" }}
+                className="CTA-container"
             >
                 <Stack
-                    component="section"
-                    direction={{ xs: "column-reverse", sm: "row" }}
-                    className="CTA-container"
+                    className="CTA-imagePanel imagePanel"
+                    alignItems="center"
+                    justifyContent="center"
                 >
-                    <Stack
-                        className="CTA-imagePanel imagePanel"
-                        alignItems="center"
-                        justifyContent="center"
-                    >
-                        <Box
-                            component="img"
-                            src="/images/banner01.jpg">
-                        </Box>
-                        <Stack className="CTA-addRecipeLink" alignItems={"center"} justifyContent={"center"}>
-                            <Link component={RouterLink} to="/recipes/create" underline="none">
-                                <IconButton                                >
-                                    <FontAwesomeIcon icon={faPlusCircle} />
-                                </IconButton>
-                            </Link>
-                            <Typography variant="body1">
-                                Create a new recipe
-                            </Typography>
-                        </Stack>
+                    <Box
+                        component="img"
+                        src="/images/banner01.jpg">
+                    </Box>
+                    <Stack className="CTA-addRecipeLink" alignItems={"center"} justifyContent={"center"}>
+                        <Link component={RouterLink} to="/recipes/create" underline="none">
+                            <IconButton                                >
+                                <FontAwesomeIcon icon={faPlusCircle} />
+                            </IconButton>
+                        </Link>
+                        <Typography variant="body1">
+                            Create a new recipe
+                        </Typography>
+                    </Stack>
 
-                    </Stack>
-                    <Stack
-                        className="CTA-headlinePanel headlinePanel-right"
-                        direction="column"
-                        alignItems="center"
-                        justifyContent="center"
-                        spacing={2}
-                    >
-                        <Typography component="h1" variant="h1">
-                            Let's get Cookin'.
-                        </Typography>
-                        <Typography variant="body2">
-                            Copy/Paste recipe text from anywhere to build beautifully formatted recipes!
-                        </Typography>
-                    </Stack>
                 </Stack>
-            </Box>
-    )
-
+                <Stack
+                    className="CTA-headlinePanel headlinePanel-right"
+                    direction="column"
+                    alignItems="center"
+                    justifyContent="center"
+                    spacing={2}
+                >
+                    <Typography component="h1" variant="h1">
+                        Let's get Cookin'.
+                    </Typography>
+                    <Typography variant="body2">
+                        Copy/Paste recipe text from anywhere to build beautifully formatted recipes!
+                    </Typography>
+                </Stack>
+            </Stack>
+        </Box>
+    );
 
     return (
         <div className="Homepage">
@@ -120,24 +121,45 @@ function Homepage({ register }: props) {
             {/* HERO SECTION */}
             <Stack
                 component="section"
-                className="Hero1"
-                direction={{ xs: "column", sm: "row" }}
+                className="Hero"
+                direction="column"
                 alignItems="center"
                 justifyContent="center"
                 sx={{
-                    backgroundImage:`url('/images/alt-banner.jpg')`
+                    backgroundImage: `url('/images/alt-banner-02.jpg')`
                 }}
             >
                 <Stack
                     className="Headline-container"
                     direction="column"
+                    alignItems="center"
+                    justifyContent='center'
                     spacing={2}
                 >
-                    <Typography component="h1" variant="h1">
-                        Recipes for the home cook
+                    <Typography component="h1" variant="h1"
+                        sx={{
+                            fontSize: { 'md': '5.5rem', 'sm': '4rem', 'xs': '3rem' }
+                        }}
+                    >
+                        The digital recipe book for home chefs
                     </Typography>
-
+                    <IconButton
+                        className="Hero-next"
+                        sx={{
+                            fontSize: { 'md': '4rem', 'sm': '3.5rem', 'xs': '3rem' }
+                        }}
+                        onClick={() => {
+                            window.scrollTo({
+                                behavior: "smooth",
+                                top: formatSection.current!.offsetTop + 200,
+                            });
+                        }}
+                    >
+                        <FontAwesomeIcon icon={faCircleArrowDown} />
+                    </IconButton>
                 </Stack>
+                <Box width={'80%'}>
+                </Box>
                 <Stack
                     className="Hero-more"
                     alignItems="center"
@@ -149,94 +171,99 @@ function Homepage({ register }: props) {
                 </Stack>
             </Stack>
 
-            {/* HERO SECTION */}
+            {/* FORMAT SECTION */}
             <Stack
                 component="section"
-                className="Hero"
+                id="format"
+                className="Format"
+                ref={formatSection}
                 direction={{ xs: "column", sm: "row" }}
+                sx={{
+                    backgroundImage: 'url(/images/hero-banner.jpg)'
+                }}
             >
                 <Stack
-                    className="Hero-headlinePanel headlinePanel-left"
+                    className="Format-headlinePanel headlinePanel-left"
                     direction="column"
                     alignItems="center"
                     justifyContent="center"
                     spacing={2}
                 >
                     <Typography component="h1" variant="h1">
-                        Recipes should be easy to use
+                        Never lose your place again
                     </Typography>
                     <Typography variant="body2">
-                        Recipes have had the same format for over a century, but the way we cook has changed.  It’s time for a layout that works for the home chef.
+                        Parsley uses AI to reformat your favorite recipes, putting each ingredient right next to the instructions where it is required.
                     </Typography>
                 </Stack>
                 <Stack
-                    className="Hero-imagePanel imagePanel"
+                    className="Format-contents"
                     alignItems="center"
                     justifyContent="center"
                 >
-                    <Box
-                        component="img"
-                        src="/images/hero-banner.jpg">
-                    </Box>
-                    {/* <Box className="Homepage-animation"> */}
                     <Lottie loop animationData={explainerAnimation} className="Homepage-animation" />
-                    {/* </Box> */}
-
                 </Stack>
             </Stack>
 
             {/* Features section */}
             <Stack
-                className="Features"
-                alignItems="center"
                 justifyContent="center"
-                component="section"
-                spacing={3}
-                direction={{ xs: "column", sm: "row" }}
+                alignItems="center"
+                className="Features"
             >
-                <Box className="Features-feature">
-                    <Box
-                        className="Features-featureIcon"
-                        component="img"
-                        src="/images/plate_01.png"
-                    />
-                    <Typography component='h3' variant='h3'>
-                        Create
-                    </Typography>
-                    <Typography variant='body1'>
-                        Copy/paste any recipe and we’ll format it for you.
-                    </Typography>
-                </Box>
-                <Box className="Features-feature">
-                    <Box
-                        className="Features-featureIcon"
-                        component="img"
-                        src="/images/plate_02.png"
-                    />
-                    <Typography component='h3' variant='h3'>
-                        Save
-                    </Typography>
-                    <Typography variant='body1'>
-                        Save your favorite recipes to your favorites for future meals.                    </Typography>
-                </Box>
-                <Box className="Features-feature">
-                    <Box
-                        className="Features-featureIcon"
-                        component="img"
-                        src="/images/plate_03.png"
-                    />
-                    <Typography component='h3' variant='h3'>
-                        Cook
-                    </Typography>
-                    <Typography variant='body1'>
-                        Bring Parsley into the kitchen and enjoy an easier way to cook.                    </Typography>
-                </Box>
 
+                <Typography variant="h1" component="h2">
+                    Three ways to add your favorite recipes
+                </Typography>
+                <Box
+                    className="Features-grid"
+                    component="section"
+                >
+                    <Link component={RouterLink} to="/recipes/create" className="Features-feature">
+                        <Box
+                            className="Features-featureIcon"
+                            component="img"
+                            src="/images/plate_01.png"
+                        />
+                        <Typography component='h3' variant='h3'>
+                            Text
+                        </Typography>
+                        <Typography variant='body1'>
+                            copy/paste from anywhere to build beautifully formatted recipes                        </Typography>
+                    </Link>
+
+                    <Link component={RouterLink} to="/recipes/create" className="Features-feature">
+                        <Box
+                            className="Features-featureIcon"
+                            component="img"
+                            src="/images/plate_02.png"
+                        />
+                        <Typography component='h3' variant='h3'>
+                            Image
+                        </Typography>
+                        <Typography component='p' variant='body1'>
+                            Snap a photo of grandma’s recipe book to quickly digitize recipes                    </Typography>
+                    </Link>
+
+                    <Link component={RouterLink} to="/recipes/create" className="Features-feature">
+                        <Box
+                            className="Features-featureIcon"
+                            component="img"
+                            src="/images/plate_03.png"
+                        />
+                        <Typography component='h3' variant='h3'>
+                            AI
+                        </Typography>
+                        <Typography variant='body1'>
+                            Ask our robot chef to cook up something special just for you                    </Typography>
+                    </Link>
+
+                </Box>
             </Stack>
 
             {/* CTA SECTION */}
 
-        {username ? userCTA : anonCTA}
+            {username ? userCTA : anonCTA}
 
 
 
